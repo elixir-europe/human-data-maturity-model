fa91d6aa6290de343299edc78d3f4aa72437e717
Martin Cook <martin.cook@elixir-europe.org>
2022-03-29 17:41:16 +0100

    Tidy Javascript for accordions

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,31 +1,17 @@
 
 window.addEventListener('load', (event) => {
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     console.log(this.nextElementSibling);
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
-//   });
-// }
-
-const accordionHeaders = document.querySelectorAll('[data-accordion-header]');
-console.log(accordionHeaders);
-Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
-  let target = accordionHeader.parentElement.nextElementSibling;
-  accordionHeader.onclick = () => {
-    let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
-    accordionHeader.setAttribute('aria-expanded', !expanded);
-    target.hidden = expanded;
-  }
-})
+  /* Accordion script for accessible accordions.
+  *  Credit: https://www.aditus.io/patterns/accordion/
+  */
+  const accordionHeaders = document.querySelectorAll('[data-accordion-header]');
+  Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
+    let target = accordionHeader.parentElement.nextElementSibling;
+    accordionHeader.onclick = () => {
+      let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
+      accordionHeader.setAttribute('aria-expanded', !expanded);
+      target.hidden = expanded;
+    }
+  })
 
 });
